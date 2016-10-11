6940abd251ae7e057ba25f10211a94baabe10ee7
GabrielPerson <gabrielperson.pm@gmail.com>
2016-10-11 10:01:41 -0300

    Commit 1 -  main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+//
+// Created by person on 11/10/16.
+//
+
+/*TRABALHO PRATICO 2 AEDSIII - CAMINHANDO NA FISICA (GRAFOS)
+ *
+ *  ENTRADAS: N, M, T -> TAMANHO DA MATRIZ (NxM) E NUMERO DE CHAVES AO MESMO TEMPO
+ *  REPRESENTACAO MAPA (ENTRADA) -> '.' - POSICOES LIVRE
+ *                                  '#' - OBSTACULOS
+ *                                  'c','d,'h,'s' - CHAVES
+ *                                  'C','D,'H,'S' - PORTAS
+ *                                  'V' - POSICAO INICIAL DE VINICIUS
+ *                                  'E' - SAIDA DA FISICA
+ *                                  'X,Y' - 0<X,Y<9 (POSICAO FINAL DO BURADO DE MINHOCA)
+ *
+ *  SAIDA - TEMPO MINIMO PARA SAIR DA FISICA (SE IMPOSSIVEL - IMPRIMIR -1)
+ *
+ */
+
+
+
+int main(){
+
+
+
+    return 0;
+}
